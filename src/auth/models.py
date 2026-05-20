611479f377f14949325504e12f7c7369b0639450
Joao Pedro <joaopioonze@gmail.com>
2026-05-20 12:13:44 -0300

    feat:cria tabela e conexao com o banco de dados

---
+++ src/auth/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.auth.database import Base
+
+
+class Url(Base):
+    __tablename__ = "url"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    original_url: Mapped[str] = mapped_column(String, nullable=False)
+    short_url: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    created_date: Mapped[str] = mapped_column(
+        DateTime, default=datetime.now(), nullable=False
+    )
+    expires_date: Mapped[str] = mapped_column(DateTime, nullable=False)
+    click_count: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
