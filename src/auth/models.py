67256eec92754b03da3188944f3c499f25820183
Joao Pedro <joaopioonze@gmail.com>
2026-05-25 11:55:54 -0300

    feat: adiciona uma foreignkey  na tabela url e cria tabela user

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, DateTime, Integer, String
+from sqlalchemy import Boolean, DateTime, Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.auth.database import Base
@@ -20,3 +20,15 @@ class Url(Base):
     expires_date: Mapped[str] = mapped_column(DateTime, nullable=False)
     click_count: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
